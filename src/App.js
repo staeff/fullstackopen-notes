0159f5ff832bd5dd5cf35f464d965f0f0fa44d51
Stephan Klinger <staeff@arcor.de>
2021-08-12 23:03:16 +0200

    Give each child a key

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = ({notes}) => {
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li>{note.content}</li>)}
+        {notes.map(note => <li key={note.id}>{note.content}</li>)}
       </ul>
     </div>
   )
