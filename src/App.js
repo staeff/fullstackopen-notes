fe3b1efe79acb2d0b1c7cf8f80b79efef4afa459
Stephan Klinger <staeff@arcor.de>
2021-08-12 23:16:58 +0200

    Move note to own component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react'
 
+const Note = ({ note }) => {
+  return (
+    <li>{note.content}</li>
+  )
+}
+
+
 const App = ({notes}) => {
 
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => <li key={note.id}>{note.content}</li>)}
+        {notes.map(note =>
+          <Note key={note.id} note={note} />
+        )}
       </ul>
     </div>
   )
