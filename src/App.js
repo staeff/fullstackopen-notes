168da32e5a75d8137cd2350cd1132dcfa9cf947b
Stephan Klinger <staeff@arcor.de>
2021-08-23 19:55:22 +0200

    Add new notes to the dom

--- src/App.js
+++ src/App.js
@@ -4,11 +4,29 @@ import Note from './components/Note'
 const App = (props) => {
   // pass array of notes to useState to
   // initialize the piece of state stored in notes
-  const [notes, setNodes] = useState(props.notes)
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState('a new note...')
 
+  // function to create new notes
   const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
+    const noteObject = {
+      // receives content from the components newNote state
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5, // 50% chance that a note is marked important
+      id: notes.length + 1,
+    }
+
+    // Added to the list of notes using the array method concat
+    // to create a copy and not mutate the original object
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
   }
 
   return (
@@ -20,7 +38,9 @@ const App = (props) => {
         )}
       </ul>
       <form onSubmit={addNote}>
-        <input type="text" />
+        <input type="text"
+               value={newNote}
+               onChange={handleNoteChange} />
         <button type='submit'>save</button>
       </form>
     </div>
