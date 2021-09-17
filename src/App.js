d4e8d30c7402b141fd4229c509a4119366b2dcd6
Stephan Klinger <staeff@arcor.de>
2021-09-17 12:34:32 +0200

    Hide error msg on first page load

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
-  const [errorMessage, setErrorMessage] = useState('some error happened')
+  const [errorMessage, setErrorMessage] = useState(null)
 
   // An example of an effect hook
   // a call to a state-updating function (setNotes) triggers the re-rendering
